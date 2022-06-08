98fbbade2a852cb3d5b693611caa28edf04d0623
mafesoares <mfsoares00@gmail.com>
2022-06-08 17:29:00 -0300

    Criação de método para adicção de cliente

--- src/main/java/com/api/helpr/services/ClienteService.java
+++ src/main/java/com/api/helpr/services/ClienteService.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api.helpr.domain.Cliente;
+import com.api.helpr.domain.Pessoa;
+import com.api.helpr.domain.dtos.ClienteDTO;
 import com.api.helpr.repositories.ClienteRepository;
 import com.api.helpr.repositories.PessoaRepository;
+import com.api.helpr.services.exceptions.DataIntegrityViolationException;
 import com.api.helpr.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,6 +34,27 @@ public class ClienteService {
 		return repository.findAll();
 	}
 
+	//Método que adiciona um novo cliente
+	public Cliente create(ClienteDTO objDto) {
+		objDto.setId(null);
+		validaCpfEEmail(objDto);
+		Cliente newObj = new Cliente(objDto);
+		return repository.save(newObj);
+	}
+	
+	//Método que valida os CPFs e emails para update e create
+	private void validaCpfEEmail (ClienteDTO objDto) {
+		
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDto.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDto.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");		
+		}
+		
+		obj = pessoaRepository.findByEmail(objDto.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDto.getId()) {
+			throw new DataIntegrityViolationException("Email já cadastrado no sistema!");		
+		}
+	}
 	
 	
 }
