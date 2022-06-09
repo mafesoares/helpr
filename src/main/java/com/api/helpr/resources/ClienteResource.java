3f9978a947c8ae785c9e8fa9c0cc1a3791768a2c
mafesoares <mfsoares00@gmail.com>
2022-06-09 15:25:19 -0300

    Criação de método para deleter clientes

--- src/main/java/com/api/helpr/resources/ClienteResource.java
+++ src/main/java/com/api/helpr/resources/ClienteResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,11 +63,11 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(new ClienteDTO(obj));
 	}
 	
-	
-	
-	
-	
-	
-	
-	
+	//Exclusão de clientes
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClienteDTO> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+		
 }
