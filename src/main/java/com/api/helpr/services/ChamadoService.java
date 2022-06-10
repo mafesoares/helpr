cb34cda7ac53a07f11c1581518b0965c656d6827
mafesoares <mfsoares00@gmail.com>
2022-06-10 15:41:32 -0300

    RestController para listagem de todos os chamados

--- src/main/java/com/api/helpr/services/ChamadoService.java
+++ src/main/java/com/api/helpr/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.api.helpr.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class ChamadoService {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado não foi encontrado: " + id));
 		
+	}
+	
+	public List<Chamado> findAll(){
+		return repository.findAll();
 	}
 }
